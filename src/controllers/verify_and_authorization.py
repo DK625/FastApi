78fb62d645571c26e4a1ac652bd8027c6ea3d831
root <root@PC1144>
2023-04-12 08:56:57 +0700

    fastapi

--- src/controllers/verify_and_authorization.py
+++
@@ -1,8 +0,0 @@
-import jwt
-from flask import current_app, request
-
-
-def Middleware():
-    token = request.headers["Authorization"]
-    current_user = jwt.decode(token, current_app.config["SECRET_KEY"], algorithms=["HS256"])
-    return current_user
